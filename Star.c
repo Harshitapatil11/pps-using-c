c288950ac7b2336eed081b191b5eb699be86eb0b
Harshitapatil11 <harshitapatil887@gmail.com>
2025-06-20 12:55:27 +0500

    Star.c

---
+++ Star.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,space;
+    int n=4;
+    for(a=1;a<=n;a++)
+    {
+        for(space=1;space<=n-a;space++)
+        {
+            printf(" ");
+        }
+        for(b=1;b<=2*a-1;b++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
